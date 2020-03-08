d80b7a7801ec5ef03c58dc7f954e5172cc34fc41
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-08 20:52:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 def my_collect(argument)
+  new = [] 
+  i = 0 
+  while i < argument.length 
+    new << yield(argument[i])
+    i+=1
+  end
+  new 
 end
+
+
+my_collect(argument) do 
+  i = 0 
+  while i < argument.length 
+    argument[i].split(" ").first
+  end
+end
+  
