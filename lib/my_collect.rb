9bdac21fc92d76cf86cf5c00d3af1445d95a249a
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-08 21:05:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+argument = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+
+def my_collect(argument)
+  new_array = []
+  i = 0
+  while i < argument.length
+    new_array << yield(argument[i])
+    i+=1
+    end
+    new_array
+end
 
 
+my_collect(argument) do
+i = 0
+while i < argument.length
+  argument[i].split(" ").first
+    i+=1
+  end
+  argument
+end
